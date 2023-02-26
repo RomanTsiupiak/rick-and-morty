a77c894a1949cd0395fefeaed7d303fb297bf794
Roman Tsiupiak <romantsiupiak@gmail.com>
2023-02-26 17:05:38 +0200

    test deploy action

--- src/Pages/Characters.tsx
+++ src/Pages/Characters.tsx
@@ -4,7 +4,7 @@ import { CharactersCard } from '../Components/CharactersCard';
 const Characters = () => {
   return (
     <div>
-      <div className="text-center text-2xl text-[#5368ED]">Rick</div>
+      <div className="text-center text-2xl text-[#5368ED]">Rick and Morty</div>
       <CharactersCard
         name="Rick"
         status="Alive"
