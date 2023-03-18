5f5d655ba44cbf512925205d14fa6f0a18dca8ec
Roman Tsiupiak <romantsiupiak@gmail.com>
2023-03-18 23:02:39 +0200

    feat: add mock service setup

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,38 @@ import AppProviders from 'Pages/App/AppProviders';
 import App from 'Pages/App/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <AppProviders>
-      <App />
-    </AppProviders>
-  </React.StrictMode>
-);
+// Render methods for msw
+async function render() {
+  if (process.env.REACT_APP_ENABLE_MSW) {
+    if (window.location.pathname === process.env.PUBLIC_URL) {
+      window.location.pathname = `${process.env.PUBLIC_URL}/`;
+      return;
+    }
+
+    // eslint-disable-next-line
+    const { worker } = require('test/browser');
+
+    await worker.start({
+      waitUntilReady: true,
+      serviceWorker: {
+        url: `${process.env.PUBLIC_URL}/mockServiceWorker.js`,
+      },
+    });
+  }
+
+  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+  root.render(
+    <React.StrictMode>
+      <AppProviders>
+        <App />
+      </AppProviders>
+    </React.StrictMode>
+  );
+}
+
+render();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitalsv
 reportWebVitals();
