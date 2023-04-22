43965bd0dffbaad340bd329843f063a3990e33bc
Roman Tsiupiak <romantsiupiak@gmail.com>
2023-04-22 20:44:30 +0300

    feat: add coming soon page

--- src/Pages/App/App.tsx
+++ src/Pages/App/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Sidebar from 'Components/Sidebar/Sidebar';
 import { PageLayout } from 'Components/PageLayout';
+import ComingSoon from 'Components/ComingSoon';
 import Characters from 'Pages/Characters/Characters';
 
 import './styles.css';
@@ -12,7 +13,16 @@ const App = () => {
     <div className="w-screen h-screen bg-[#F5F5F5] flex">
       <Sidebar />
       <Routes>
-        <Route path="/dashboard" element={<PageLayout title="Dashboard" />} />
+        {/* DASHBOARD */}
+        <Route
+          path="/dashboard"
+          element={
+            <PageLayout title="Dashboard">
+              <ComingSoon />
+            </PageLayout>
+          }
+        />
+        {/* CHARACTERS */}
         <Route
           path="/characters"
           element={
@@ -21,9 +31,32 @@ const App = () => {
             </PageLayout>
           }
         />
-        <Route path="/characters/:id" element={<PageLayout title="Profile" />} />
-        <Route path="/locations" element={<PageLayout title="Locations" />} />
-        <Route path="/episodes" element={<PageLayout title="Episodes" />} />
+        <Route
+          path="/characters/:id"
+          element={
+            <PageLayout title="Profile">
+              <ComingSoon />
+            </PageLayout>
+          }
+        />
+        {/* LOCATIONS */}
+        <Route
+          path="/locations"
+          element={
+            <PageLayout title="Locations">
+              <ComingSoon />
+            </PageLayout>
+          }
+        />
+        {/* EPISODES */}
+        <Route
+          path="/episodes"
+          element={
+            <PageLayout title="Episodes">
+              <ComingSoon />
+            </PageLayout>
+          }
+        />
         <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </div>
